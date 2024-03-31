d3cd4185af0ff1b9ee57a1235077d1a4c4862f77
komal kumari <kumarikomal43220@gmail.com>
2024-03-31 20:41:41 +0530

    new changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,5 +11,10 @@ accountCity = "Delhi"
 
 console.log(accountId);
 
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 console.table ([accountId, accountEmail, accountPassword, accountCity])
 
