bb49dd326717550c3f09ee51850cdecbd27e9db5
komal kumari <kumarikomal43220@gmail.com>
2024-03-31 21:14:55 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strit"; // treat all JS code as newer version
+
+
+// alert (3 +3) // we are using node js not broswer
+
+console.log(3
+    + 3) // code readability should be high
+
+console.log("Komal")
+
+let name = "Komal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+//symbol => unique
+
+
+
+
+// object 
+
+
+console.log(typeof "Komal");
+
+console.log(typeof age);
+
+
+console.log(typeof undefined); //undefinedd
+console.log(typeof null); // object
