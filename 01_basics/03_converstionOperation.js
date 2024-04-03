9e6ceedef034d847f609e3794ca1e98daf70b574
komal kumari <kumarikomal43220@gmail.com>
2024-04-03 22:42:23 +0530

    coversion operations

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -26,3 +26,37 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ------------------------- Operations -----------------------
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+
+let str1 = "hello "
+let str2 = "komal"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(3 + 4 * 5 % 3);
+
+let gameCounter = 100
+++ gameCounter;
+console.log(gameCounter);
